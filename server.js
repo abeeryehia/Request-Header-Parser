2adf5cff2bab5d3a5b5aa4b179a14e4c2d1a1ad3
Abeer <a.yehia@fci-cu.edu.eg>
2016-07-31 09:05:28 +0000

    project done

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ var inofObj = {
 app.set('port', (process.env.PORT || 5000))
 app.enable('trust proxy');
 app.get('/',function(req,res){
-    userIp = req.ip;//req.headers['x-forwarded-for']//||req.connection.remoteAddress;;
+    //userIp = req.ip;//req.headers['x-forwarded-for']//||req.connection.remoteAddress;;
     //userLang = req.headers["accept-language"];
     //var ua = req.headers['user-agent'];
     //var from = ua.indexOf('(')+1;
     //var to = ua.indexOf(')');
     //userSW = ua.substr(from,to-from);
-    inofObj.ipaddress = userIp;
+    //inofObj.ipaddress = userIp;
     //inofObj.language = userLang;
     //inofObj.software= userSW;
-    res.send(inofObj);
+    //res.send(inofObj);
+    res.send('Hello');
 });
 
 app.listen(app.get('port'));
